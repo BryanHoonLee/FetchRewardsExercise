30f4852256a819e0b8e260511c7c9fd3c4ccc193
Bryan <bryanhoonlee@gmail.com>
2021-08-08 19:18:43 -0700

    First Commmit

---
+++ app/src/main/java/com/hoonstudio/fetchrewardscodingexercise/model/FetchRewardsSchema.kt
@@ -0,0 +1,12 @@
+package com.hoonstudio.fetchrewardscodingexercise.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class FetchRewardsItem(
+    val id: String = "",
+    val listId: String = "",
+    val name: String = ""
+): Parcelable
